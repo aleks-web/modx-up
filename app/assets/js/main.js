3f0100c85dcf9f6c4ab8112e6b33fcd7f67e6478
Alexey Antropov <dok.go@yandex.ru>
2023-03-14 20:40:27 +0300

    first

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,23 +1,6 @@
 /*
     desc: Универсальный скрипт раскрытия/скрытия списка. Реализовано для БЭМ блока blocks/_drop-listing.scss
 */
-// $(function() {
-//     $('.drop-listing__current').click(function(e) {
-//         $(this).siblings('.drop-listing__list').toggleClass('drop-listing__list--open');
-//         $(this).find('.drop-listing__arrow').toggleClass('drop-listing__arrow--open');
-//     });
-
-//     $('.drop-listing__list').each(function(e) {
-//         $(this).mouseleave(function(e) {
-//             $(this).toggleClass('drop-listing__list--open');
-//             $(this).parent().find('.drop-listing__arrow').toggleClass('drop-listing__arrow--open');
-//         })
-//     });
-// });
-
-
-
-
 $(function() {
 
     $('.drop-listing').each(function(e) {
